4657e007fdc495acb71b80d8c191322177c4a08a
doomvell13 <wknight13@gmail.com>
2021-06-19 10:55:45 +0800

    fix bugs for heroku deployment

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/google/callback',
+        callbackURL: 'https://trainernetic.herokuapp.com/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
